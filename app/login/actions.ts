ccb52fa7f98809eca9839c6c87fd063a1755d75f
Andres Uriel Aguirre Ocampo <andres.aguirre_o@hotmail.com>
2025-06-02 10:34:04 -0600

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -6,29 +6,38 @@ export async function loginUser(formData: FormData) {
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
-  const res = await fetch(`https://hugin-api.vercel.app/auth/login/`, {
-    method: 'POST',
-    body: JSON.stringify({ email, password }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  if (!res.ok) {
-    let message = 'Acceso denegado por los dioses';
-    try {
-      const data = await res.json();
-      message = data.message || message;
-    } catch (error) {
-      console.error('Error parsing response:', error);
-    throw new Error(message);
+  try {
+    const res = await fetch(`https://hugin-api.vercel.app/auth/login/`, {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (!res.ok) {
+      let message = 'Acceso denegado por los dioses';
+      try {
+        const data = await res.json();
+        message = data.message || message;
+      } catch (error) {
+        console.error('Error parsing response:', error);
+      throw new Error(message);
+    }
+  }
+  
+    const data = await res.json();
+  
+    const cookieStore = await cookies();
+    cookieStore.set('token', data.token, {
+      httpOnly: true,
+      path: '/',
+    });
+  
+    return true;
+  } catch (error) {
+    console.error('Error in loginUser:', error);
+    throw new Error('Error processing login');
+    
   }
 
-  const data = await res.json();
 
-  const cookieStore = await cookies();
-  cookieStore.set('token', data.token, {
-    httpOnly: true,
-    path: '/',
-  });
-
-  return true;
 }
